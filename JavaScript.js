fe03ddada063b66a77f013db4c08f9ba24ac8194
Colleen Risso <alecandogwalker@alec-rissos-macbook-pro.local>
2018-08-25 10:10:03 -0700

    created cards & card array

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,37 @@
+let pic1 = document.createElement("img");
+img.src = "pic1";
+var src = document.getElementById("x");
+src.appendChild(img);
+let pic2 = document.createElement("img");
+img.src = "pic2";
+var src = document.getElementById("x");
+src.appendChild(img);
+let pic3 = document.createElement("img");
+img.src = "pic3";
+var src = document.getElementById("x");
+src.appendChild(img);
+let pic4 = document.createElement("img");
+img.src = "pic4";
+var src = document.getElementById("x");
+src.appendChild(img);
+let pic5 = document.createElement("img");
+img.src = "pic5";
+var src = document.getElementById("x");
+src.appendChild(img);
+let pic6 = document.createElement("img");
+img.src = "pic6";
+var src = document.getElementById("x");
+
+let cards = [pic1,pic1,pic2,pic2,pic3,pic3,pic4,pic4,pic5,pic5,pic6,pic6];
+
+
+
+
+
+
+
+
+
+
+
 
