11d1f071197acf3e519078d769bf25ecc184e79a
Colleen Risso <alecandogwalker@alec-rissos-macbook-pro.local>
2018-08-25 12:24:12 -0700

    card updates

--- JavaScript.js
+++ JavaScript.js
@@ -1,27 +1,27 @@
 //cards
 let pic1 = document.createElement("img");
 img.src = "pic1.jpg";
-var src = document.getElementById("x");
+var src = document.getElementById("cardStyle1");
 src.appendChild(img);
 let pic2 = document.createElement("img");
 img.src = "pic2.jpg";
-var src = document.getElementById("x");
+var src = document.getElementById("cardStyle2");
 src.appendChild(img);
 let pic3 = document.createElement("img");
 img.src = "pic3.jpg";
-var src = document.getElementById("x");
+var src = document.getElementById("cardStyle3");
 src.appendChild(img);
 let pic4 = document.createElement("img");
 img.src = "pic4.jpg";
-var src = document.getElementById("x");
+var src = document.getElementById("cardStyle4");
 src.appendChild(img);
 let pic5 = document.createElement("img");
 img.src = "pic5.jpg";
-var src = document.getElementById("x");
+var src = document.getElementById("cardStyle5");
 src.appendChild(img);
 let pic6 = document.createElement("img");
 img.src = "pic6.jpg";
-var src = document.getElementById("x");
+var src = document.getElementById("cardStyle6");
 
 let cards = [pic1,pic1,pic2,pic2,pic3,pic3,pic4,pic4,pic5,pic5,pic6,pic6];
 
