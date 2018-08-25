406b85cb0495e824be25eeebe725de4d2fa7c999
Carolyn <ccpianist@gmail.com>
2018-08-25 10:11:25 -0700

    first draft shuffle funciton

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,30 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function shuffle(cards) {
+    let x = null;
+    //if eventListener startButton...
+    for(let i = 0; i < cards.length; i++){
+        //generates random number rounded down 0-11:
+        x = math.floor(math.random() * 12);
+        output += '<div id="card' + x + '"></div>';
+        return board.innerHTML = output;
+    }
+}
